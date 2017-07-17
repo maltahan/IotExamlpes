c235f0667225f402b348b1089394eb541d0a56e4
Mohammad <mohammad.altahan@sharedop.com>
2017-07-17 15:06:52 +0200

    add the project to the repository

---
+++ Lesson_201/StartSolution/MainPage.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+namespace Lesson_201
+{
+    /// <summary>
+    /// The application main page.  Because we are running headless we will not see anything
+    /// even though it is begin generated at runtime.  This acts as the main entry point for the 
+    /// application functionality.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        // This method will be called by the application framework when the page is first loaded.
+        protected override async void OnNavigatedTo(NavigationEventArgs navArgs)
+        {
+            Debug.WriteLine("MainPage::OnNavigatedTo");
+
+        }
+
+        /// <summary>
+        // This method will put your pin on the world map of makers using this lesson.
+        // This uses imprecise location (for example, a location derived from your IP 
+        // address with less precision such as at a city or postal code level). 
+        // No personal information is stored.  It simply
+        // collects the total count and other aggregate information.
+        // http://www.microsoft.com/en-us/privacystatement/default.aspx
+        // Comment out the line below to opt-out
+        /// </summary>
+        public void MakePinWebAPICall()
+        {
+            try
+            {
+                HttpClient client = new HttpClient();
+
+                // Comment this line to opt out of the pin map.
+                client.GetStringAsync("http://adafruitsample.azurewebsites.net/api?Lesson=201");
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine("Web call failed: " + e.Message);
+            }
+        }
+
+    }
+}
